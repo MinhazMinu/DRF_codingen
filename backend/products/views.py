23fabac80119dae66f72b9b15417246edc6ad9cf
Minhazminu <minhazul.taher@gmail.com>
2023-05-31 12:55:20 +0600

    Django Rest Framework Generics RetrieveAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductDetailsAPIView(generics.RetrieveAPIView):
+    """DRF API View"""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = "pk"
+    # lookup_url_kwarg = "product_id"
---
+++ py_client/detail.py
@@ -0,0 +1,8 @@
+import requests
+
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/products/2"
+
+get_response = requests.get(endpoint)
+print(get_response.status_code)
+print(get_response.json())
