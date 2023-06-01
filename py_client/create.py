d8e136c5444db0432c3c4d00a300ea27ec77b711
Minhazminu <minhazul.taher@gmail.com>
2023-06-01 11:58:32 +0600

    Django Rest Framework ListAPIView & ListCreateAPIView

--- py_client/create.py
+++ py_client/create.py
@@ -6,4 +6,4 @@ endpoint = "http://127.0.0.1:8000/api/products/"
 data = {"title": "Test tile", "price": 123.45}
 get_response = requests.post(endpoint, json=data)
 print(get_response.status_code)
-# print(get_response.json())
+print(get_response.json())
