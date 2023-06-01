97ec5b7b96f5c6b56db358d7ad29333a0b6c8fa3
Minhazminu <minhazul.taher@gmail.com>
2023-06-01 12:48:12 +0600

    Using Function Based Views For Create Retrieve or List

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:pk>/", views.ProductDetailsAPIView.as_view()),
-    path("", views.ProductListCreateAPIView.as_view()),
+    # path("", views.ProductListCreateAPIView.as_view()),
+    # path("<int:pk>/", views.ProductDetailsAPIView.as_view()),
+    path("", views.product_alt_view),
+    path("<int:pk>/", views.product_alt_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import generics
 
 from .models import Product
@@ -25,3 +28,29 @@ class ProductDetailsAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
     # lookup_field = "pk"
     # lookup_url_kwarg = "product_id"
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        queryset = Product.objects.all(raise_exception=True)
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors)
