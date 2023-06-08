e686e96c79c9408fc516b77f4937fabb0485667a
Minhazminu <minhazul.taher@gmail.com>
2023-06-08 12:07:47 +0600

    ViewSets & Routers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path("/auth/", obtain_auth_token, name="api_token_auth"),
+    path("auth/", obtain_auth_token, name="api_token_auth"),
     path("", views.api_home, name="home"),
     # path('api/', apiOverview, name="api-overview"),
 ]
